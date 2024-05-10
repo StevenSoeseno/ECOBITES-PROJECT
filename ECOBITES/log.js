0f41c6546ad86191915743f29208c7796002449b
StevenSoeseno <118049593+StevenSoeseno@users.noreply.github.com>
2024-05-10 23:00:02 +0700

    Delete ECOBITES directory

--- ECOBITES/log.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("typePW");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
